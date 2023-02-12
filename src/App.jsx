da742f784f4e1db15641298ed066343ab22cdff2
sakas19 <your@email.comsarvanz.kasaei@edu.cmeducation.se>
2023-02-12 16:35:41 +0100

    newcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useState } from 'react'
 import './App.css'
+import me from './assets/me.jpg'
+
+
+function getActiveClassName (activeArticle, currentArticle) {
+  if (activeArticle ===  currentArticle) {
+    return 'show-article'
+  } else {
+    return 'hide-article'
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeArticle, setActiveArticle] = useState('First Article')
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <article className={`active ${getActiveClassName(activeArticle, 'First Article')}`} >
+        <h1>About Me</h1>
+        <p>Sarvnaz är 25 år gammal innan dess hade pluggat till sjuksköterska och jobbar vid sidan av sina studie hos Ikea, en av Sarvnaz bästa vän märkte att sarvnaz mår inte bra under sina studie och rekommderarde att hon testar programmering. hon gillade atta kunna skapa websidor och jobba både med kreativitet samt probemlösning då hon slutade från universitet och började plugga frontend-development hos changemaker education  .</p>
+        <img className='me' src={me} alt="" />
+      </article>
+      <article className={`active ${getActiveClassName(activeArticle, '2nd Article')}`} >
+        <h1>Idea</h1>
+        <p>Jag ska göra en simple version av the magic 8-ball. Jag hade gjort ett liknande project som nybörjare det bli intressant att jag göra det igen med mina nuvarande kunskap</p>
+      </article>
+      <article className={`active ${getActiveClassName(activeArticle, '3rd Article')}`} >
+      <h1>Design</h1>
+      <p>Jag ska köra en simple design fast med mycket färg.</p>
+      </article>
+      <div className='article'>
+        <button onClick={() => {
+          setActiveArticle((a) => {
+            if (a === 'First Article') {
+              return '2nd Article'
+            } else {
+              return 'First Article'
+            }
+          })
+        }}>Switch Article</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
